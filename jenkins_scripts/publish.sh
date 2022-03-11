b8132811a83e5035419776d2e112ad031581b6da
shivinquartic <shivin@quartic.ai>
2022-03-11 13:10:27 +0530

    QPD-1979: Updated with pip tools

--- jenkins_scripts/publish.sh
+++ jenkins_scripts/publish.sh
@@ -11,4 +11,12 @@ echo "$BRANCH_NAME"
 export PUBLISH_PYPI=true
 export GITHUB_TOKEN="$GIT_TOKEN"
 
+pip install -U pip
+pip install pip-tools
+
+echo "creating requirements"
+bash ./jenkins_scripts/update_dependency.sh $BRANCH_NAME
+
+cat requirements.txt
 pip install -r requirements.txt
+
